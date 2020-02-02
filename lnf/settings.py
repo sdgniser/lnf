15e5f5d66b33d21d11b5eb745c668c353d6f6ddc
Software Development Group <sdg@niser.ac.in>
2020-02-02 23:15:13 +0530

    fixed path for media and static files

--- lnf/settings.py
+++ lnf/settings.py
@@ -100,11 +100,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/lnf/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/lnf/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 from .local_settings import *
