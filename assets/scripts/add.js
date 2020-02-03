1c076c6c02f9eab27d609f1a390fb9c4b5638699
K. Sannidhya Shukla <feynhat@gmail.com>
2020-02-03 06:30:20 +0530

    Other js

---
+++ assets/scripts/add.js
@@ -0,0 +1,24 @@
+function main() {
+	let selectCategory = document.querySelector('#id_category');
+	selectCategory.onchange = function(e) {
+		if (selectCategory.value === "Other") {
+			selectCategory.setAttribute("name", "");
+
+			let inputCategory = document.createElement("input");
+			inputCategory.id = "id_category_other";
+			inputCategory.setAttribute("type", "text");
+			inputCategory.setAttribute("name", "category");
+			inputCategory.setAttribute("required", "");
+
+			selectCategory.parentNode.appendChild(inputCategory);
+		} else {
+			let inputCategory = document.querySelector("#id_category_other");
+			if (inputCategory) {
+				inputCategory.parentNode.removeChild(inputCategory);
+				selectCategory.setAttribute("name", "category");
+			}
+		}
+	};
+}
+
+window.onload = main;
